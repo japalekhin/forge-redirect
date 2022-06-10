93244006000e95f215a2763f1c9776f685112775
Japa Alekhin Llemos <jap@alekhin.io>
2022-06-10 21:09:35 +0800

    Formatted scripts.
    Signed-off-by: Japa Alekhin Llemos <jap@alekhin.io>

--- public/index.php
+++ public/index.php
@@ -1,19 +1,20 @@
 <?php
-    // Initialize composer autoload and Dotenv package
-    require __DIR__ . '/../vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..');
-    $dotenv->load();
 
-    // Get the .env variables
-    $domain = rtrim(getenv('REDIRECT_TO'), '/');
-    $append = filter_var(getenv('REDIRECT_WITH_REQUEST_URI'), FILTER_VALIDATE_BOOLEAN);
+// Initialize composer autoload and Dotenv package
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..');
+$dotenv->load();
 
-    // Build the new url to redirect to
-    $new_url = rtrim($domain, '/');
-    if ($append) {
-        $new_url .= getenv('REQUEST_URI');
-    }
+// Get the .env variables
+$domain = rtrim(getenv('REDIRECT_TO'), '/');
+$append = filter_var(getenv('REDIRECT_WITH_REQUEST_URI'), FILTER_VALIDATE_BOOLEAN);
 
-    // Permanent 301 redirection
-    header("HTTP/1.1 301 Moved Permanently");
-    header('Location: ' . $new_url);
+// Build the new url to redirect to
+$new_url = rtrim($domain, '/');
+if ($append) {
+    $new_url .= getenv('REQUEST_URI');
+}
+
+// Permanent 301 redirection
+header("HTTP/1.1 301 Moved Permanently");
+header('Location: ' . $new_url);
